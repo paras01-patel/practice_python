f6bb2284c0cf14f2a92abea533ddf1f81d373e6f
paras01-patel <parschandrawanshi649@gmail.com>
2026-02-04 21:21:42 +0530

    practice_python

---
+++ baisc.py
@@ -0,0 +1,219 @@
+# 1.postive nagative and zero
+
+# n=int(input("enter number :"))
+
+# if n>0:
+#     print("postive")
+# elif n<0:
+#     print("negative")
+# else:
+#     print("zero")
+    
+    
+
+
+# 2.even/oddd
+
+# n =int(input("enter a number"))
+
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# 3. gretest two number
+
+# n=int(input("enter a number"))
+# m=int(input("enter a number"))
+
+# if n>m:
+#     print("first is greater")
+# elif n ==m:
+#     print("first and second are equal")
+# else:
+#     print("second is greater")
+    
+    
+# 4.greatest of three number 
+
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# c = int(input("enter a number"))
+
+# if a>b and a>c:
+#     print("a is greater")
+# elif b>a and b>c:
+#     print("b is grester")
+# else:
+#     print("c is greater")
+
+# 5.voting eliglibity
+
+# age=int(input("enter a number"))
+
+# if age >=18:
+#     print("you can vote")
+# else:
+#     print("you can vote")
+
+
+# 6 leap year
+
+# year=int(input("enter a number"))
+
+# if ( year%4==0 and year%100!=0) or (year%400==0):
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+
+
+# 7. dvisible 5 and 11
+
+# n=int(input("enter a number"))
+
+# if (n%5==0 and n%11==0):
+#     print("divisible 5 and 11")
+# else:
+#     print("not divisible 5 and 11")
+    
+# 8. vowel and consonant
+
+# ch=input("enter a character :").lower
+# if ch in "aeiou":
+#     print("vowel")
+# else:
+#     print("consonant")
+    
+    
+# 9.upper case and lower case 
+
+# ch = input("enter a character :")
+
+# if ch.upper:
+#     print("upper")
+# else:
+#     print("lower")
+
+# 10.pass or fail
+
+# mark=int(input("enter a number"))
+
+# if mark >=33:
+#     print("pass")
+# else:
+#     print("fail")
+
+# 11. grade System
+
+
+# mark=int(input("enter a number"))
+
+# if (mark>=80 and mark<=100):
+#     print("grade A")
+# elif (mark>=60 and mark<=79):
+#     print("garde B")
+# elif (mark>=33 and mark<=59):
+#     print("grade C")
+# else:
+#     print("fail")
+
+# 12. divisible by 3 and 5 
+
+
+# n=int(input("enter a number"))
+# if n%3==0 and n%5==0:
+#     print("divisible bu 3 and 5 ")
+# else :
+#     print("divisible by 3 and 5")
+    
+# 13. valid triangle
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a number"))
+
+
+# if a+b+c == 180:
+#     print("valid triangle")
+# else:
+#     print("not a triangle")    
+
+
+
+
+
+
+
+
+# loops for 
+
+# 1 . print 1 to 10
+
+# for i in range(1,11):
+#     print(i)
+
+# 2.print number from 10 to 1
+
+# for i in range(-10,1):
+#     print(i)
+
+# 3.print even number 1 to 20
+
+# for i in range(1,20):
+#     if i%2==0:
+#         print(i)
+
+# 4.print odd number 1 to 20
+
+
+# for i in range(1,20):
+#     if i%2!=0:
+#         print(i)
+
+# 5.print sqare of number 1 to 10 
+
+# for i in range(1,11):
+#     print(i,"sqaure",i*i)
+
+# 6.count total in string 
+
+# test="python" 
+# count=0
+
+# for ch in test:
+#     count+=1
+#     print(count)
+
+# 7.print each elemet ofa list 
+
+# l=[10,20,30,40,50]
+# for i in l:
+#     print(i)
+
+# 8.find sum of elwment list 
+
+# l=[10,20,30,40,50]
+# total=0
+
+# for i in l:
+#     total+=i
+# print(total)
+
+
+# 9. find max  number in a list
+# l=[10,20,30,40,50]
+# max=l[0] 
+# for i in l:
+#     if i > max:
+#         max=i
+#         print(max)
+
+
+# 10 .Multiplication table of a number
+
+# num =5
+# for i in range(1,11):
+#     print(num*i)
+
+
